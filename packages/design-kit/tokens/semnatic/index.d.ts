48661f1a3a3568653c9f695da4756829c4a61919
zegiha <leeseoyul070814@gmail.com>
2025-09-20 22:49:19 +0900

    ✨ add design semantic

---
+++ packages/design-kit/tokens/semnatic/index.d.ts
@@ -0,0 +1,71 @@
+type LabelTokens = {
+  strong: string
+  normal: string
+  weak: string
+}
+
+type ContainerTokens = {
+  even: string
+  odd: string
+}
+
+type FillTokens = {
+  normal: string
+  weak: string
+}
+
+type LineTokens = {
+  normal: string
+}
+
+type BlackTokens = {
+  static: string & {
+    '64': string
+  }
+  variable: string
+}
+
+type WhiteTokens = {
+  static: string
+  variable: string & {
+    '00': string
+    '24': string
+  }
+}
+
+type InteractionTokens = {
+  hover: string
+}
+
+type AccentTokens = {
+  gray: string
+  red: string
+  pink: string
+  blue: string
+  yellow: string
+  green: string
+  translucent: {
+    gray: string
+    red: string
+    pink: string
+    blue: string
+    yellow: string
+    green: string
+  }
+}
+
+type ThemeTokens = {
+  label: LabelTokens
+  container: ContainerTokens
+  fill: FillTokens
+  line: LineTokens
+  black: BlackTokens
+  white: WhiteTokens
+  interaction: InteractionTokens
+  accent: AccentTokens
+}
+
+export declare const semantic: {
+  light: ThemeTokens
+  dark: ThemeTokens
+}
