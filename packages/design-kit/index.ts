1fe52f7e7a3035398448fa9403da8c1162d17149
zegiha <leeseoyul070814@gmail.com>
2025-09-23 14:31:03 +0900

    ✨ add font to design-kit/typography

--- packages/design-kit/index.ts
+++ packages/design-kit/index.ts
@@ -1,4 +1,4 @@
 export { palette } from './tokens/palette'
 export { semantic } from './tokens/semnatic'
 export { radius } from './tokens/radius'
-export { typography } from './tokens/typography'
+export { typography, pretendard, jetbrainsMono } from './tokens/typography'
--- packages/design-kit/tokens/typography/index.ts
+++ packages/design-kit/tokens/typography/index.ts
@@ -2,6 +2,15 @@ import './default/default.css'
 import './index.css'
 
 import {typography as typographyDefault} from './default/default'
+import localFont from 'next/font/local'
+
+export const pretendard = localFont({
+  src: '../../assets/PretendardVariable.woff2'
+})
+
+export const jetbrainsMono = localFont({
+  src: '../../assets/JetBrainsMono-Medium.woff2'
+})
 
 export const typography = {
   default: typographyDefault
