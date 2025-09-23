5cca40944202a2c60dd9ea1cf965acf572ee0dee
zegiha <leeseoyul070814@gmail.com>
2025-09-23 17:10:40 +0900

    ✨ add optimistic ui process to changeTheme and changeThemeMode

--- packages/design-kit/helper/chageThemeMode.ts
+++ packages/design-kit/helper/chageThemeMode.ts
@@ -2,7 +2,21 @@ import { ThemeModeEnum } from '../const/ThemeModeEnum'
 import { getThemeFromClient } from './getThemeFromClient'
 
 export async function changeThemeMode(themeMode: ThemeModeEnum) {
+  let prevThemeMode: Array<string> | undefined
+
   try {
+    if(window !== undefined) {
+      const html = document.querySelector('html')
+      if(!html) throw new Error('window is not undefined but html is undefined')
+
+      const theme = getThemeFromClient()
+
+      prevThemeMode = Array.from(html.classList)
+
+      html.classList.remove(...(Object.values(ThemeModeEnum).map(mode => `${theme}-${mode}`)))
+      html.classList.add(`${theme}-${themeMode}`)
+    }
+
     await fetch(`/api/theme/mode`, {
       method: 'POST',
       headers: {
@@ -10,17 +24,18 @@ export async function changeThemeMode(themeMode: ThemeModeEnum) {
       },
       body: JSON.stringify({ themeMode })
     })
+  } catch(e) {
+    console.error(e)
 
-    if(window !== undefined) {
+    alert('테마 모드를 바꾸는데 실패했어요')
+
+    if(prevThemeMode !== undefined && window !== undefined) {
       const html = document.querySelector('html')
       if(!html) throw new Error('window is not undefined but html is undefined')
 
-      const theme = getThemeFromClient()
-
-      html.classList.remove(...(Object.values(ThemeModeEnum).map(mode => `${theme}-${mode}`)))
-      html.classList.add(`${theme}-${themeMode}`)
+      prevThemeMode.forEach((v) => {
+        html.classList.add(v)
+      })
     }
-  } catch(e) {
-    console.error(e)
   }
 }
--- packages/design-kit/helper/changeTheme.ts
+++ packages/design-kit/helper/changeTheme.ts
@@ -2,20 +2,16 @@ import { ThemeEnum } from "../const/ThemeEnum"
 import { getThemeModeFromClient } from "./getThemeModeFromClient"
 
 export async function changeTheme(theme: ThemeEnum) {
-  try {
-    await fetch('/api/theme', {
-      method: 'POST',
-      body: JSON.stringify({ theme }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+  let prevTheme: Array<string> | undefined = undefined
 
+  try {
     if(window != undefined) {
       const html = document.querySelector('html')
       if(!html) throw new Error('window is not undefined, but html is undefined')
 
       const themeMode = getThemeModeFromClient()
+
+      prevTheme = Array.from(html.classList)
 
       html.classList.remove(...(Object.values(ThemeEnum).map(v => `${v}-palette`)))
       html.classList.remove(...(Object.values(ThemeEnum).map(v => `${v}-radius`)))
@@ -23,7 +19,26 @@ export async function changeTheme(theme: ThemeEnum) {
       html.classList.remove(...(Object.values(ThemeEnum).map(v => `${v}-${themeMode}`)))
       html.classList.add(`${theme}-palette`, `${theme}-radius`, `${theme}-typography`, `${theme}-${themeMode}`)
     }
+
+    await fetch('/api/theme', {
+      method: 'POST',
+      body: JSON.stringify({ theme }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   } catch(e) {
     console.error(e)
+
+    alert('테마를 바꾸지 못했어요')
+
+    if(prevTheme !== undefined && window !== undefined) {
+      const html = document.querySelector('html')
+      if(!html) throw new Error('window is not undefined, but html is undefined')
+
+     prevTheme.forEach((v) => {
+       html.classList.add(v)
+     })
+    }
   }
 }
