0bfc2f2225ee0bf309dc9acb8777ab93ed7fb558
zegiha <leeseoyul070814@gmail.com>
2025-09-25 11:39:15 +0900

    :package: delete lucide, lucideLab package

--- packages/design-kit/index.ts
+++ packages/design-kit/index.ts
@@ -1,11 +1,8 @@
-import "./lucide.css";
-
-export { palette } from "./tokens/palette";
-export { semantic } from "./tokens/semnatic";
-export { radius } from "./tokens/radius";
-export { typography, pretendard, jetbrainsMono } from "./tokens/typography";
-
 export type { TypeWidth } from "./tokens/layout/width/types";
+export { palette } from "./tokens/palette";
+export { radius } from "./tokens/radius";
+export { semantic } from "./tokens/semnatic";
+export { jetbrainsMono, pretendard, typography } from "./tokens/typography";
 
 import widthModuleCSS from "./tokens/layout/width/width.module.css";
 export { widthModuleCSS };
