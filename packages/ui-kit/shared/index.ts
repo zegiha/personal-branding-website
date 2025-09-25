07f2e49e2d23ee8a95882e852abae4ea9265ced0
zegiha <leeseoyul070814@gmail.com>
2025-09-25 13:59:28 +0900

    :bug: fix folder path bug from shared/index.ts

--- packages/ui-kit/shared/index.ts
+++ packages/ui-kit/shared/index.ts
@@ -1,5 +1,5 @@
 import uiKitTagUnit from "./uiKitTagUnit.module.css";
 export { uiKitTagUnit };
 
-export { getCSSVariableByUiKitContentColor } from "./helper";
-export type { TypeUiKitContentColor } from "./type";
+export type { TypeUiKitContentColor } from "./uiKitContentColor";
+export { getCSSVariableByUiKitContentColor } from "./uiKitContentColor";
