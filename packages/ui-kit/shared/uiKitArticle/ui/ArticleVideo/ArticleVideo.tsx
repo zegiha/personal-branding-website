c7ab6c44a322b056c3520ac5d7efd0f19eff6893
zegiha <leeseoyul070814@gmail.com>
2025-10-13 11:51:58 +0900

    :sparkles: make article video to support youtube link

--- packages/ui-kit/shared/uiKitArticle/ui/ArticleVideo/ArticleVideo.tsx
+++ packages/ui-kit/shared/uiKitArticle/ui/ArticleVideo/ArticleVideo.tsx
@@ -2,10 +2,14 @@ import { Col, Typo } from "../../../../foundation";
 import type { TypeArticleVideo } from "../../type";
 import st from "./style.module.css";
 
-export function ArticleVideo({ url, caption }: TypeArticleVideo) {
+export function ArticleVideo({ url, format, caption }: TypeArticleVideo) {
   return (
     <Col className={st.container} gap={8} alignItems="center">
-      <video src={url} controls className={st.video} />
+      {format === "youtube" ? (
+        <iframe className={st.iframe} src={url} allowFullScreen></iframe>
+      ) : (
+        <video src={url} controls className={st.video} />
+      )}
       {caption && <Typo.caption.medium color="weak">{caption}</Typo.caption.medium>}
     </Col>
   );
